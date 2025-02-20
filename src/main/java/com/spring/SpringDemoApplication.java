e7bfc03eac409915280822edf0c24b007c4cc3b4
mustafagabi <mustafakabi712@gmail.com>
2025-02-20 14:43:15 +0300

    Learn About Structure Of Project

--- src/main/java/com/spring/SpringDemoApplication.java
+++ src/main/java/com/spring/SpringDemoApplication.java
@@ -3,11 +3,23 @@ package com.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+سوف يتم استبدالهم مع @SpringBootApplication
+*/
+
+//@SpringBootApplication(scanBasePackages = {"controller"}) في حال كان كونتروبر خارج الباكج
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args); // this is bootstrap for spring boot
+		/*
+		1- يقوم بعمل كونتير
+		2- يقو بتحميل Tomcat ويشغل التطبيق عليه
+		 */
 	}
 
 }
@@ -19,4 +31,4 @@ Folder resources
 JAR diff WAR
  تختلف بينهم هيكلة المشروع واهم اختلاف ملفات التي شرحناها سابقا
 
- */
+*/
--- src/main/java/com/spring/controller/TestingController.java
+++ src/main/java/com/spring/controller/TestingController.java
@@ -1,7 +1,9 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // https://http://localhost:8080
@@ -10,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 // https://http://localhost:8080/api
 public class TestingController
 {
+    @Value(value = "${admin.name}") // تستخدم لتحميل قيمة من ملف application.properties
+    private String name;
+
     @GetMapping("name")
 // https://http://localhost:8080/api/name
     public String getName(){
-        return "I am Mustafa Kabi";
+        return "I am " + name;
     }
+    @GetMapping("/add")
+    // Endpoint جديد لجمع عددين: http://localhost:8080/api/add
+    public String addNumbers(@RequestParam String num1, @RequestParam String num2) {
+        return num1 + num2; // إرجاع ناتج جمع العددين
+    }
+
 }
